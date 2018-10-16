748815318a5f833131f72581113484eaf6337d58
Brandon <theeternalvigil@gmail.com>
2018-10-16 14:30:56 -0700

    created JSON for slider images, added styles for image containers, fixed bug for columnContainers formatting

--- src/aboutAuthor.js
+++ src/aboutAuthor.js
@@ -4,11 +4,10 @@ import './AboutAuthor.css';
 class aboutAuthor extends Component {
 	render () {
 		return (
-		<div className='autherBio'>
-		<div className='authorImageContainer'>
-			<img className='authorImage' src='/' alt='' title='' />
-		</div>
-		Check this guy out.
+		<div className='authorBio'>
+			<div className='authorImageContainer'>
+				<img className='authorImage' src='https://vignette.wikia.nocookie.net/snk/images/3/35/Ngbc-kyo-select-portrait-b.png/revision/latest?cb=20130704020920' alt='' title='Placeholder Image' />
+			</div>
 		</div>
 		);
 	}
--- src/mainHero.js
+++ src/mainHero.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './mainHero.css';
+import sliderImages from './sliderImages.json';
 
 class mainHero extends Component {
 	constructor(props) {
@@ -7,9 +8,15 @@ class mainHero extends Component {
 		this.state = { content: 'normal' };
 		console.log('hero state: ' + this.state.content);
 	}
+
+	componentDidMount () {
+		console.log(sliderImages);
+	}
+
 	render () {
 		return (
 			<div className = 'mainHero'>
+				<img className='heroBanner' src='https://www.strikeforcepest.com/wp-content/uploads/2014/01/Lightning-storm-banner-cropped.jpg' href='' alt='' title='There is a storm coming...' />
 			</div>
 			);
 	}
